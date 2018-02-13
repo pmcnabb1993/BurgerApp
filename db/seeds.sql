131f2c14f694f829a9a434ec5a787e07b78a4ec6
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-02-13 15:56:11 -0600

    Updated the schema and seeds for the mysql database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Hunger Buster', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Pretty Patty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Royal With Cheese', false);
