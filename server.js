04bfc37639c8e9cc81de8fc5c628af26fd309c8d
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-02-19 00:06:57 -0600

    Fixed issues with the connection to mysql

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require('method-override');
-var mysql = require('mysql');
 
 var PORT = process.env.PORT || 3000;
 
@@ -17,8 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(methodOverride("_method"));
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
@@ -26,9 +21,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require('./controllers/burgerController.js');
+var routes = require("./controllers/burgerController.js");
 
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
